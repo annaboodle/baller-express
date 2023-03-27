4b1fac71d113ea56dd2bb9ebeeff37ff4aeafe21
Annabeth Carroll <annabethcarroll@gmail.com>
2023-03-27 13:57:48 -0700

    add more pix and fix node-sass issue

--- src/photoMap.js
+++ src/photoMap.js
@@ -5,6 +5,7 @@ import Angus from "./img/avatars/Angus.png";
 import Anna from "./img/avatars/Anna.png";
 import Annabeth from "./img/avatars/Annabeth.png";
 import Anshul from "./img/avatars/Anshul.png";
+import Azure from "./img/avatars/Azure.jpg";
 import Bailey from "./img/avatars/Bailey.png";
 import Beverly from "./img/avatars/Bev.png";
 import Bob from "./img/avatars/Bob.png";
@@ -15,6 +16,7 @@ import ChrisButler from "./img/avatars/ChrisButler.png";
 import ChrisHayes from "./img/avatars/ChrisHayes.png";
 import Courtney from "./img/avatars/Courtney.png";
 import DavidPaparelli from "./img/avatars/DavidPaparelli.jpg";
+import Dikul from "./img/avatars/Dikul.jpg";
 import Ellen from "./img/avatars/Ellen.png";
 import Emily from "./img/avatars/Emily.png";
 import Emmy from "./img/avatars/Emmy.png";
@@ -25,6 +27,7 @@ import Hansel from "./img/avatars/Hansel.png";
 import Harrison from "./img/avatars/Harrison.png";
 import James from "./img/avatars/James.jpg";
 import Joi from "./img/avatars/Joi.png";
+import Kavishi from "./img/avatars/Kavishi.jpg";
 import Kristina from "./img/avatars/Kristina.png";
 import Lea from "./img/avatars/Lea.png";
 import Maggie from "./img/avatars/Maggie.png";
@@ -39,6 +42,7 @@ import PapaHoff from "./img/avatars/PapaHoff.JPG";
 import Rachel from "./img/avatars/Rachel.png";
 import Rob from "./img/avatars/Rob.png";
 import Rohan from "./img/avatars/Rohan.png";
+import Sam from "./img/avatars/Sam.png";
 import Sarah from "./img/avatars/Sarah.png";
 import Saumil from "./img/avatars/Saumil.png";
 import Saurin from "./img/avatars/Saurin.png";
@@ -46,6 +50,7 @@ import Scout from "./img/avatars/Scout.png";
 import ScottWoolf from "./img/avatars/ScottWoolf.png";
 import Shanni from "./img/avatars/Shanni.png";
 import Steven from "./img/avatars/Steven.png";
+import Tara from "./img/avatars/Tara.jpg";
 import Vaibhav from "./img/avatars/Vaibhav.png";
 import Vanu from "./img/avatars/Vanu.png";
 import Will from "./img/avatars/Will.png";
@@ -61,16 +66,18 @@ export const photoMap = {
   Anna,
   Annabeth,
   Anshul,
+  Azure,
   Bailey,
   Beverly,
   Bob,
   Brian,
   Catherine,
-  Chris,
+  "Chris Lorenz": Chris,
   "Chris Butler": ChrisButler,
   "Chris Hayes": ChrisHayes,
   Courtney,
   "David Paparelli": DavidPaparelli,
+  Dikul,
   Ellen,
   Emily,
   Emmy,
@@ -81,6 +88,7 @@ export const photoMap = {
   Harrison,
   James,
   Joi,
+  Kavishi,
   Kristina,
   Lea,
   Maggie,
@@ -95,6 +103,7 @@ export const photoMap = {
   Rachel,
   Rob,
   Rohan,
+  Sam,
   Sarah,
   Saumil,
   Saurin,
@@ -102,6 +111,7 @@ export const photoMap = {
   "Scott Woolf": ScottWoolf,
   Shanni,
   Steven,
+  Tara,
   Vaibhav,
   Vanu,
   Will,
